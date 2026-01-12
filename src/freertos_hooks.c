fac268d12e3685c846d4c21f3b920bcac6990b3f
Lokesh Kumar [codetit4n] <hi@loke.sh>
2026-01-12 19:26:00 +0530

    restructure

--- src/freertos_hooks.c
+++ src/freertos_hooks.c
@@ -1,16 +1,16 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName) {
-  (void)xTask;
-  (void)pcTaskName;
-  taskDISABLE_INTERRUPTS();
-  for (;;)
-    ;
+void vApplicationStackOverflowHook(TaskHandle_t xTask, char* pcTaskName) {
+	(void)xTask;
+	(void)pcTaskName;
+	taskDISABLE_INTERRUPTS();
+	for (;;)
+		;
 }
 
 void vApplicationMallocFailedHook(void) {
-  taskDISABLE_INTERRUPTS();
-  for (;;)
-    ;
+	taskDISABLE_INTERRUPTS();
+	for (;;)
+		;
 }
--- src/main.c
+++ src/main.c
@@ -5,40 +5,40 @@
 
 #include "board.h"
 
-static void led_task(void *arg) {
-  (void)arg;
+static void led_task(void* arg) {
+	(void)arg;
 
-  for (;;) {
-    board_led1_toggle();
-    vTaskDelay(pdMS_TO_TICKS(500));
-  }
+	for (;;) {
+		board_led1_toggle();
+		vTaskDelay(pdMS_TO_TICKS(500));
+	}
 }
 
 int main(void) {
-  /* Basic board init (GPIO only for now) */
-  board_led1_init();
+	/* Basic board init (GPIO only for now) */
+	board_led1_init();
 
-  /* Create LED task */
-  BaseType_t ok = xTaskCreate(led_task, /* Task function */
-                              "LED",    /* Name (for debug) */
-                              128,      /* Stack size (words, not bytes) */
-                              NULL,     /* Parameters */
-                              1,        /* Priority */
-                              NULL      /* Task handle */
-  );
+	/* Create LED task */
+	BaseType_t ok = xTaskCreate(led_task, /* Task function */
+				    "LED",    /* Name (for debug) */
+				    128,      /* Stack size (words, not bytes) */
+				    NULL,     /* Parameters */
+				    1,	      /* Priority */
+				    NULL      /* Task handle */
+	);
 
-  /* If task creation failed, halt */
-  if (ok != pdPASS) {
-    taskDISABLE_INTERRUPTS();
-    for (;;)
-      ;
-  }
+	/* If task creation failed, halt */
+	if (ok != pdPASS) {
+		taskDISABLE_INTERRUPTS();
+		for (;;)
+			;
+	}
 
-  /* Start scheduler (never returns on success) */
-  vTaskStartScheduler();
+	/* Start scheduler (never returns on success) */
+	vTaskStartScheduler();
 
-  /* Should never reach here */
-  taskDISABLE_INTERRUPTS();
-  for (;;)
-    ;
+	/* Should never reach here */
+	taskDISABLE_INTERRUPTS();
+	for (;;)
+		;
 }
