1ab10bd81f1caeed8300d5f9164ad72e545e1c2b
Lokesh Kumar [codetit4n] <hi@loke.sh>
2026-08-16 00:08:01 +0530

    bug fixes wip: more diagnostics level shifter on reader could be the culprit for invalid holding of MISO even when device unselected

--- src/ports/w5500_port.c
+++ src/ports/w5500_port.c
@@ -2,6 +2,7 @@
 #include "FreeRTOS.h" // IWYU pragma: keep
 #include "board.h"
 #include "drivers/spi.h"
+#include "modules/logger.h"
 #include "modules/net.h"
 #include "semphr.h"
 #include "wizchip_conf.h"
@@ -51,16 +52,48 @@ void w5500_cris_exit(void) {
 	xSemaphoreGive(w5500_mutex);
 }
 
+static void busy_wait_ms(uint32_t ms) {
+	volatile uint32_t count;
+
+	while (ms--) {
+		count = 64000;
+
+		while (count--) {
+			__asm volatile("nop");
+		}
+	}
+}
+
 int8_t w5500_init(void) {
+	logger_log_literal_len("W5500 INIT:",
+		(uint8_t)(sizeof("W5500 INIT:") - 1),
+		"MUTEX",
+		(uint8_t)(sizeof("MUTEX") - 1));
+
 	w5500_mutex = xSemaphoreCreateMutexStatic(&w5500_mutex_buf);
 	configASSERT(w5500_mutex);
 
+	logger_log_literal_len("W5500 INIT:",
+		(uint8_t)(sizeof("W5500 INIT:") - 1),
+		"SPI DEVICE",
+		(uint8_t)(sizeof("SPI DEVICE") - 1));
+
 	spi_device_init(&w5500_dev);
+
+	logger_log_literal_len("W5500 INIT:",
+		(uint8_t)(sizeof("W5500 INIT:") - 1),
+		"CALLBACKS",
+		(uint8_t)(sizeof("CALLBACKS") - 1));
 
 	reg_wizchip_cs_cbfunc(cs_select, cs_deselect);
 	reg_wizchip_spi_cbfunc(w5500_spi_readbyte, w5500_spi_writebyte);
 	reg_wizchip_spiburst_cbfunc(w5500_spi_readburst, w5500_spi_writeburst);
 	reg_wizchip_cris_cbfunc(w5500_cris_enter, w5500_cris_exit);
+
+	logger_log_literal_len("W5500 INIT:",
+		(uint8_t)(sizeof("W5500 INIT:") - 1),
+		"RESET LOW",
+		(uint8_t)(sizeof("RESET LOW") - 1));
 
 	// RESET pin setup
 	GPIO_CNF(W5500_RST_PIN) = (1 << 0) | // DIR = Output
@@ -71,12 +104,50 @@ int8_t w5500_init(void) {
 
 	pin_low(W5500_RST_PIN);
 	vTaskDelay(pdMS_TO_TICKS(10));
+
+	logger_log_literal_len("W5500 INIT:",
+		(uint8_t)(sizeof("W5500 INIT:") - 1),
+		"RESET HIGH",
+		(uint8_t)(sizeof("RESET HIGH") - 1));
+
 	pin_high(W5500_RST_PIN);
 
-	// Let W5500 finish power-up before first access
+	TickType_t t_before = xTaskGetTickCount();
+
+	logger_log_uint_len("W5500 TICK BEFORE:",
+		(uint8_t)(sizeof("W5500 TICK BEFORE:") - 1),
+		&t_before,
+		sizeof(t_before));
+
+	logger_log_literal_len("W5500 INIT:",
+		(uint8_t)(sizeof("W5500 INIT:") - 1),
+		"POWERUP WAIT",
+		(uint8_t)(sizeof("POWERUP WAIT") - 1));
+
 	vTaskDelay(pdMS_TO_TICKS(150));
+	busy_wait_ms(150);
+
+	TickType_t t_after = xTaskGetTickCount();
+
+	logger_log_uint_len("W5500 TICK AFTER:",
+		(uint8_t)(sizeof("W5500 TICK AFTER:") - 1),
+		&t_after,
+		sizeof(t_after));
+
+	logger_log_literal_len("W5500 INIT:",
+		(uint8_t)(sizeof("W5500 INIT:") - 1),
+		"WIZCHIP START",
+		(uint8_t)(sizeof("WIZCHIP START") - 1));
 
 	uint8_t txsize[8] = {2, 2, 2, 2, 2, 2, 2, 2};
 	uint8_t rxsize[8] = {2, 2, 2, 2, 2, 2, 2, 2};
-	return wizchip_init(txsize, rxsize);
+
+	int8_t ret = wizchip_init(txsize, rxsize);
+
+	logger_log_literal_len("W5500 INIT:",
+		(uint8_t)(sizeof("W5500 INIT:") - 1),
+		"WIZCHIP DONE",
+		(uint8_t)(sizeof("WIZCHIP DONE") - 1));
+
+	return ret;
 }
